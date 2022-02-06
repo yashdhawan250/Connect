28e7946785e5bf6025d3e62becf5e6653077b8f0
yash dhawan <yashdhawan@yashs-MacBook-Pro.local>
2022-02-07 04:13:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+const port = 8001;
+const bodyParser = require('body-parser');
+
+
+app.listen(port,function(err){
+    if(err) throw err;
+    console.log('server is running on port '+port);
+});
