b6ada73748fae89183eda0b90c5ec50a1f835e5f
yash dhawan <yashdhawan@yashs-MacBook-Pro.local>
2022-02-08 06:39:16 +0530

    Added home router

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8001;
 const bodyParser = require('body-parser');
 
 
+
+
+app.use('/', require('./routes'));
+
 app.listen(port,function(err){
     if(err) throw err;
     console.log('server is running on port '+port);
